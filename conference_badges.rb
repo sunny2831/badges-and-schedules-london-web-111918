6356d6ed750373359723d02c33675753ea9913d9
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-06 18:37:39 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |name|
     puts name
   end
-  assign_rooms(attendees).each do |name, i|
-    return name
+  assign_rooms(attendees).each do |name|
+    puts name
   end
 end
