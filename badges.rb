30a280fd2a89566c676b4f60006b681b96da74ce
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-06 17:30:13 +0000

    Done.

---
+++ badges.rb
@@ -0,0 +1,8 @@
+integers = [1, 2, 3, 4, 5]
+
+puts integers
+puts integers.inspect
+
+integers.each do |e|
+    puts e
+end
--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(attendees)
+  attendees.each.with_index(1).collect do |name, i|
+    "Hello, #{name}! You'll be assigned to room #{i}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |name|
+    puts name
+  end
+  assign_rooms(attendees).each do |name, i|
+    return name
+  end
+end
