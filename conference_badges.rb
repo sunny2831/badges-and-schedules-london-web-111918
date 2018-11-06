a9e100b279793225a4ce6e05be68b5d8daf918b0
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-06 18:32:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 require 'pry'
-=======
-require pry
-
->>>>>>> 565013f9ba6dc42e5990191a1f745c7ca07db318
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
@@ -23,17 +18,9 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |name|
-<<<<<<< HEAD
     puts name
   end
-  assign_rooms(attendees).each do |name, i|
-    return name
-=======
-    binding.pry
-   "Hello, my name is #{name}."
-  end
-  assign_rooms(attendees).each do |name, i|
-     "Hello, #{name}! You'll be assigned to room #{i}!"
->>>>>>> 565013f9ba6dc42e5990191a1f745c7ca07db318
+  assign_rooms(attendees).each do |name|
+    puts name
   end
 end
